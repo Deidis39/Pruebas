43cc0c6682eb164d9fb4c868481787ac110120b9
Deiber <deiber601@gmail.com>
2024-12-01 16:43:57 -0400

    se le agregó la hora

--- saludo-python.py
+++ saludo-python.py
@@ -4,3 +4,4 @@ print(f'Hola {nombre} como te fué ayer en el examén que tenía en la academia'
 print('Es un buen dia para estudiar programación')
 print('LA ACTUALIZACION DE HOY ESTA CORRECTA')
 print('HOY ES FERIADO')
+print('4:39')
