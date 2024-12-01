792bdfbfb5d26f1536cf08ddb023f7b99147e7a8
Deidis39 <deidis53@gmail.com>
2024-12-01 15:01:56 -0400

    Update saludo-python.py

--- saludo-python.py
+++ saludo-python.py
@@ -3,3 +3,4 @@ nombre = 'Deidis'
 print(f'Hola {nombre} como te fué ayer en el examén que tenía en la academia')
 print('Es un buen dia para estudiar programación')
 print('LA ACTUALIZACION DE HOY ESTA CORRECTA')
+print('HOY ES FERIADO')
