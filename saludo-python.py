2a7de38254406c890c9b20c97500362d96145913
Deiber <deiber601@gmail.com>
2024-12-01 10:00:33 -0400

    Se le agregó un print

--- saludo-python.py
+++ saludo-python.py
@@ -1,10 +1,5 @@
 nombre = 'Deidis'
 
 print(f'Hola {nombre} como te fué ayer en el examén que tenía en la academia')
-
-
-
-
-
-
 print('Es un buen dia para estudiar programación')
+print('LA ACTUALIZACION DE HOY ESTA CORRECTA')
